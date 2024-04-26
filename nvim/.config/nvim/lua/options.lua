92ed9d2b083bcd5bed6db173870aadf96f2344c6
gvnnn <17831785+gvnnn@users.noreply.github.com>
2024-04-26 23:11:21 +0200

    Import nvim config

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader = " "
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.colorcolumn = "80,120"
+
+vim.opt.guicursor = ""
+
+vim.opt.number = true
+vim.opt.relativenumber = true
